243340de5af8413baa42b3b41ca99015e0963c33
Renan de Carvalho Rinaldi <renan.crinaldi@gmail.com>
2023-11-05 23:34:55 -0300

    fix: correção no nome da function

--- Serverless/main.tf
+++ Serverless/main.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_lambda_function" "techchallenge_function" {
-  function_name = "TechChallenge.API"
+  function_name = "TechChallengeAPI"
   s3_bucket = var.bucket_name
   s3_key = "TechChallengeLambdaAuth.zip"
   handler = var.handler
